9ddb9a95e9d4d4a09def54dd65b796e8cd2e07da
mickski56 <dee@mick.lan>
2023-09-11 21:15:34 +0100

    joystick control rev4

--- src/wipeout/ship_player.c
+++ src/wipeout/ship_player.c
@@ -202,16 +202,16 @@ void ship_player_update_race(ship_t *self) {
 		turn_rate_target = (pow(input_state(A_RIGHT),1.2) * self->turn_rate_max);
 		if (self->angular_velocity.y <= 0) {
 			if (-turn_rate_target < self->angular_velocity.y) {	
-				self->angular_acceleration.y += -self->turn_rate;
+				self->angular_acceleration.y -= self->turn_rate;
 				printf("1R\n");
 			}
-			else if(self->angular_velocity.y > 0) {
-				self->angular_acceleration.y += -self->turn_rate * 2;
+			else if(-turn_rate_target > self->angular_velocity.y) {
+				self->angular_acceleration.y += self->turn_rate;
 				printf("3R\n");
 			}
 		}
 		else if (self->angular_velocity.y > 0) {
-			self->angular_acceleration.y += -self->turn_rate * 2;
+			self->angular_acceleration.y -= turn_rate_target * self->turn_rate * 2;
 			printf("2R\n");
 		}
 	}
