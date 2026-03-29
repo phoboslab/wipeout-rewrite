0d1f0c63e385c6584cdff945e666bd335a6a8b2c
ford-jones <fordnicholasjones@gmail.com>
2026-03-30 11:31:56 +1300

    Slightly increase the recovery-target's distance from the edge of a jump

--- src/wipeout/ship_player.c
+++ src/wipeout/ship_player.c
@@ -423,7 +423,7 @@ void ship_player_update_race(ship_t *self) {
 			}
 
 			self->section = landing;
-			self->temp_target = vec3_mulf(vec3_add(landing->center, landing->next->center), 0.5);
+			self->temp_target = vec3_mulf(vec3_add(landing->center, landing->next->center), 0.55);
 			self->temp_target.y -= 2000;
 			self->velocity = vec3(0, 0, 0);
 		}
